dd1434b72c15fbba647d388e1d5d2cc78b73e6e0
diegdar <diegdarch@hotmail.com>
2024-11-06 19:10:01 +0100

    Creados metodos AgregarClub y BuscarClub en Liga

--- equipos_futbol/Liga.cs
+++ equipos_futbol/Liga.cs
@@ -9,7 +9,35 @@ namespace equipos_futbol
     internal class Liga
     {
         List<Jugador> listaJugadores = new List<Jugador>();
-        List<Equipo> listaEquipos = new List<Equipo>();
+        List<Club> listaClubes = new List<Club>();
+
+        public void AgregarClub(Club club)
+        {
+            if (!listaClubes.Contains(club))
+                listaClubes.Add(club);
+            else
+                Console.WriteLine($"No se puede agregar el club {club.Nombre} pues ya existe en la BBDD.");
+        }
+
+        private string TratarEntradaString(string input)
+        {//Convierte la primera letra a mayusculas y el resto lo deja en minusculas, ademas de quitar espacios del principio y final
+            input = input.Trim();
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+        }
+
+        public Club BuscarClub(string nombreClub)
+        {
+            nombreClub = TratarEntradaString(nombreClub);  
+            Club clubEncontrado = listaClubes.FirstOrDefault(c => c.Nombre == nombreClub);
+
+            if (clubEncontrado == null)
+                return null;
+            else 
+                return clubEncontrado;
+        }
 
 
     }
