5d7ae9ab66f1e077f724fa67cebe76579a130c81
diegdar <diegdarch@hotmail.com>
2024-11-06 18:41:30 +0100

    Creacion clase Liga con sus atributos de clase

---
+++ equipos_futbol/Liga.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equipos_futbol
+{
+    internal class Liga
+    {
+        List<Jugador> listaJugadores = new List<Jugador>();
+        List<Equipo> listaEquipos = new List<Equipo>();
+
+
+    }
+}
