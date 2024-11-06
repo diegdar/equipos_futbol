30e5a6666a6a09f384d91bcde34cd2659b082562
Gustavo <gustavomanconi@gmail.com>
2024-11-06 18:51:39 +0100

    Creacion clases Jugador y Club

---
+++ equipos_futbol/Club.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equipos_futbol
+{
+    public class Club
+    {
+        public string Nombre { get; set; }
+        public List<Jugador> jugadores = new List<Jugador>();
+
+
+        public Club()
+        {
+
+        }
+
+        public Club(string nombre, List<Jugador> jugadores)
+        {
+            Nombre = nombre;
+            this.jugadores = jugadores;
+        }
+    }
+}
---
+++ equipos_futbol/Jugador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equipos_futbol
+{
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public string Numero { get; set; }
+        public bool Asignacion { get; set; }
+
+
+        public Jugador()
+        {
+
+        }
+        public Jugador(string nombre, string numero, bool asignacion)
+        {
+            Nombre = nombre;
+            Numero = numero;
+            Asignacion = false;
+        }
+    }
+}
